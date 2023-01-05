c817f7b29911211493016bce688263b958f478e3
Paulo Henrique <paulohsantos114@gmail.com>
2023-01-05 19:08:17 -0300

    Adicionar arquivos de projeto.

---
+++ Ex_05/Program.cs
@@ -0,0 +1,21 @@
+﻿/*4.    Faça um programa que calcule o total de lucro por produto vendido
+a.    O usuário precisa informar quando parar
+b.    E preciso armazenar na matriz Produto, Quantidade, Preço Compra e Preço de Venda
+c.    O cálculo retorna o lucro e soma em uma variável fora do loop para então exibir para o funcionário.*/
+
+namespace Programa_05
+{
+    class Programa_5
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Este é um programa para obter o lucro da venda de produtos.");
+            string[] produto = Array.Empty<string>();
+            int[] quantidadeDeProdutos = Array.Empty<int>();
+            double[] precoCompra = Array.Empty<double>();
+            double[] precoVenda = Array.Empty<double>();
+
+
+        }
+    }   
+}
