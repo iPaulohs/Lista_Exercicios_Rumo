fec784870cc4a58975d1786bd65ba934e97adc1d
iPaulohs <paulohsantos114@gmail.com>
2023-01-08 22:16:31 -0300

    Finalização dos exercícios.

---
+++ Exercicio_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*1.Faça um programa que calcula a média das notas de provas de uma turma.*/
+
+namespace Programa_02
+{
+    class Programa_2
+    {
+        public static void Main(string[] args)
+        {
+            int numeroDeNotas;
+            double somaDasnotas = 0;
+            Console.WriteLine("Este é um programa para calcular a média das notas dos alunos de uma sala. De quantas notas você deseja calcular a média?");
+            numeroDeNotas = int.Parse(Console.ReadLine());
+            double[] notas = new double[numeroDeNotas];
+
+            for (int i = 0; i < numeroDeNotas; i++)
+            {
+                Console.WriteLine("Digite a " + (i + 1) + "ª nota: ");
+                notas[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (int j = 0; j < numeroDeNotas; j++)
+            {
+                somaDasnotas += notas[j];
+            }
+
+            double media = somaDasnotas / numeroDeNotas;
+            Console.WriteLine("A média das notas declaradas é: " + media);
+        }
+    }
+}
